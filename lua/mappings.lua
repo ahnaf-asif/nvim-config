302fd862494326c5ce217a07220e20f20b2ce359
ahnaf-asif <ahnafshahriar92@gmail.com>
2025-04-08 00:57:46 +0600

    update mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,5 +6,19 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-
+map({ "i", "n" }, "sf", "<Cmd> w <cr>", { desc = "Save file" })
+map({ "i", "n" }, "sa", "gg0vG$", { desc = "Select all" })
+map({ "i", "n" }, "sb", "v%", { desc = "Select block" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--
+-- vim.keymap.set({ "n", "t" }, "hsp", function()
+--   require("nvchad.term").toggle { pos = "sp", id = "horizontalTerm" }
+-- end)
+--
+-- vim.keymap.set({ "n", "t" }, "vsp", function()
+--   require("nvchad.term").toggle { pos = "vsp", id = "verticalTerm" }
+-- end)
+--
+-- vim.keymap.set({ "n", "t" }, "", function()
+--   require("nvchad.term").toggle { pos = "float", id = "floatingTerm" }
+-- end)
